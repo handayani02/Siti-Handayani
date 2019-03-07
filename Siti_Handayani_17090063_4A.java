cb320a24f9a17c18405e27c22f94b1d48260cbc9
Siti Handayani <48308034+handayani02@users.noreply.github.com>
2019-03-07 12:44:12 +0700

    Add files via upload

---
+++ Siti_Handayani_17090063_4A.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package OOP1;
+
+/**
+ *
+ * @author HP
+ */
+public class Siti_Handayani_17090063_4A {
+     public static void main(String[] args) {
+
+        System.out.println("selamat datang di object oriented programing 1 \n siti_handayani_17090063_4A");
+    
+}
+    }
